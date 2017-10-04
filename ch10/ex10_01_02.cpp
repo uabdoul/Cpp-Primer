75cac1ee0c9bab5589262822b44d9f874bfd3c6c
Abdoul Ouedraogo <ouedraogo.abdoul.w@gmail.com>
2017-10-03 22:38:38 -0700

    Add using namespcace for better readability. Changed output display message

--- ch10/ex10_01_02.cpp
+++ ch10/ex10_01_02.cpp
@@ -19,17 +19,26 @@
 #include <vector>
 #include <algorithm>
 #include <list>
+//adding using std::vector, std::endl, std::cout 
+//for better readability.
 
+using std::vector;
+using std::cout;
+using std::list;
+using namespace std;
 
 int main()
 {
     // 10.1
-    std::vector<int> v = { 1, 2, 3, 4, 5, 6, 6, 6, 2 };
-    std::cout << "ex 10.01: " << std::count(v.cbegin(), v.cend(), 6) << std::endl;
+    // we initialize the vector list
+    vector<int> v = { 1, 2, 3, 4, 5, 6, 6, 6, 2 };
+    //output a mesage that tells how many times 6 is display
+    
+    cout<<"6 is displayed in ex 10.01: " <<std::count(v.cbegin(), v.cend(), 6) <<" times"<<endl<<endl;
 
     // 10.2
-    std::list<std::string> l = { "aa", "aaa", "aa", "cc" };
-    std::cout << "ex 10.02: " << std::count(l.cbegin(), l.cend(), "aa") << std::endl;
+    list<string> l = { "aa", "aaa", "aa", "cc" };
+    cout << "In ex 10.02: the string aa is displayed " <<std::count(l.cbegin(), l.cend(), "aa") <<" times"<<endl;
 
     return 0;
 }
